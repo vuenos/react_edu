49146a5f1fd46bec8d06f1dce6ef3e3af16d4bce
jintae kim <vuenos@naver.com>
2022-02-27 10:57:11 +0900

    add login/profile functionality

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,23 +1,47 @@
 import React, { useState } from 'react';
 import {Button, Container, Form, FormLabel} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+
+    const navigate = useNavigate();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
 
-    const userInput = {
-        email: email,
-        password: password
-    }
-    console.log(userInput)
+    const loginHandler = async (e) => {
+        e.preventDefault();
 
+        // 사용자입력값
+        const userInput = {
+            email: email,
+            password: password
+        }
+
+        // 네트워킹
+        try {
+            const { data, status } = await axios.post("http://localhost:5000/api/users/login", userInput)
+
+            // data token 로컬스토리지(브라우자) 저장
+            if (status === 200) {
+                localStorage.setItem("token", data.token)
+                navigate("/mypage")
+            }
+
+
+        } catch (error) {
+            console.log(error.message)
+        }
+
+
+    }
 
     return (
         <Container>
             <h1>Sign in</h1>
-            <Form>
+            <Form onSubmit={loginHandler}>
                 <Form.Group controlId={"email"}>
                     <FormLabel>Email address</FormLabel>
                     <Form.Control
--- src/screens/Mypage.js
+++ src/screens/Mypage.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 const Mypage = () => {
+
+    const [profile, setProfile] = useState({});
+
+
+
+    const getProfile = async () => {
+
+        const token = localStorage.getItem('token');
+        console.log(token);
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+          try {
+              const { data } = await axios.get("http://localhost:5000/api/users/profile", config)
+              setProfile(data)
+          } catch (error) {
+              //
+              console.log(error.message)
+          }
+    }
+
+    useEffect(() => {
+        getProfile();
+    }, []);
+
+
     return (
         <div>
             <h1>Mypage</h1>
+            <p>{profile.name}</p>
+            <h1>{profile.email}</h1>
         </div>
     );
 };
