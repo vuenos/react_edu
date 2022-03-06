1fd5f07b64e87e3546a21e4758bc42b4414eeb69
jintae kim <vuenos@naver.com>
2022-03-06 11:06:05 +0900

    - Product Network

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from "react-redux"
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
 import store from "./store";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,9 @@ import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-      <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import { getGoods } from "../goodsData";
+import React, {useState, useEffect} from 'react';
 import { Outlet, Link } from "react-router-dom"
+import {Table, Container, Row, Col} from "react-bootstrap";
+import axios from "axios";
 
 const Products = () => {
-    let goods = getGoods()
+
+    const [products, setProducts] = useState([]);
+
+    const getProducts = async () => {
+
+        try {
+            const { data } = await axios.get("http://localhost:5000/api/products")
+            console.log("++++++++++++++++++", data);
+            setProducts(data.products)
+        } catch (error) {
+            console.log(error.response.data.message)
+        }
+
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
 
     return (
-        <div>
+        <Container>
             <h1>Products</h1>
-            <ul>
-                {goods.result.data.map((good) => (
-                    <li>
-                        <Link
-                            to={`/products/${good.goodsno}`}
-                        >
-                            {good.goodsnm}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <Outlet />
-        </div>
+            <Row>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>CountInstock</th>
+                            <th>Brand</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {products && products.map(product => (
+                        <tr>
+                            <td>{product._id}</td>
+                            <td>{product.name}</td>
+                            <td>${product.price}</td>
+                            <td>{product.countInStock} EA</td>
+                            <td>{product.brand}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </Table>
+
+            </Row>
+
+        {/*<Container>*/}
+        {/*    <Table striped bordered hover>*/}
+        {/*        <thead>*/}
+        {/*        <tr>*/}
+        {/*            <th>#</th>*/}
+        {/*            <th>First Name</th>*/}
+        {/*            <th>Last Name</th>*/}
+        {/*            <th>Username</th>*/}
+        {/*        </tr>*/}
+        {/*        </thead>*/}
+        {/*        <tbody>*/}
+
+        {/*        <tr>*/}
+        {/*            <td>2</td>*/}
+        {/*            <td>Jacob</td>*/}
+        {/*            <td>Thornton</td>*/}
+        {/*            <td>@fat</td>*/}
+        {/*        </tr>*/}
+        {/*        <tr>*/}
+        {/*            <td>3</td>*/}
+        {/*            <td colSpan={2}>Larry the Bird</td>*/}
+        {/*            <td>@twitter</td>*/}
+        {/*        </tr>*/}
+        {/*        </tbody>*/}
+        {/*    </Table>*/}
+        {/*</Container>*/}
+        {/*<Outlet />*/}
+        </Container>
     );
 };
 
